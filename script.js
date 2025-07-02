006dcd883826551ae4d003050317863551e5a55a
NgocAnhCoder21 <anhdethuong160421@gmail.com>
2025-07-02 20:28:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,35 @@ new Swiper('.card-wrapper', {
     clickable: true,
     dynamicBullets: true
     },
+    // Navigation arrowsnew Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+    // Pagination bullets
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+    },
     // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+    // Responsive breakpoints
+    breakpoints: {
+    0: {
+    slidesPerView: 1
+    },
+    768: {
+    slidesPerView: 2
+    },
+    1024: {
+    slidesPerView: 3
+   
+   }
+   
+   }
+   });
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
