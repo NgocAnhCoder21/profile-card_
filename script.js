8da55ac5dca987ad631e8a2b0f4633506020a42a
NgocAnhCoder21 <anhdethuong160421@gmail.com>
2025-07-15 15:24:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-new Swiper('.card-wrapper', {
-    loop: true,
-    spaceBetween: 30,
-    // Pagination bullets
-    pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true
-    },
-    // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
-    // Responsive breakpoints
-    breakpoints: {
-    0: {
-    slidesPerView: 1
-    },
-    768: {
-    slidesPerView: 2
-    },
-    1024: {
-    slidesPerView: 3
-   
-   }
-   
-   }
-   });
+<!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script>
+    const swiper = new Swiper('.mySwiper', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }
+    });
+  </script>
+</body>
+</html>
